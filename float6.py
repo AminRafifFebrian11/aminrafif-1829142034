0d5c728f9564948dd840b10b95051928c84d2da5
AminRafifFebrian11 <49119429+AminRafifFebrian11@users.noreply.github.com>
2019-10-08 15:37:14 +0700

    Create float6.py

---
+++ float6.py
@@ -0,0 +1,25 @@
+def main():
+
+  print("PROGRAM PEMBAGIAN BILANGAN")
+  
+  
+  try:
+    
+      a = float(input("Masukkan a: "))
+      b = float(input("Masukkan b: "))
+      
+      hasil = a / b
+      
+  except(ZeroDivisionError, ValueError,
+         keyboardInterrupt):
+     print("\nERROR: Anda telah melakukan " +
+           "kesalahan")
+           
+else:
+  print("\na :  ", a)
+  print("b :  ", b)
+  print("a / b = ", hasil)
+  
+if __name__== "__main__":
+  main()
+  
